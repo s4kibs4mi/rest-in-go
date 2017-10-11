b8cc7180052e17f8b5940f06c89638b1d0921716
Sakib Sami <s4kibs4mi@gmail.com>
2017-10-12 04:35:27 +0600

    create contact api ready

--- api/contact.go
+++ api/contact.go
@@ -1,9 +1,31 @@
 package api
 
-import "net/http"
+import (
+	"net/http"
+	"strconv"
+	"encoding/json"
+	"rest-in-go/data"
+)
 
 func CreateContact(w http.ResponseWriter, r *http.Request) {
-
+	userId, err := strconv.Atoi(r.Header.Get("user_id"))
+	if err == nil {
+		contact := data.Contact{}
+		err := json.NewDecoder(r.Body).Decode(&contact)
+		if err == nil {
+			contact.UserId = userId
+			if contact.Save() {
+				json.NewEncoder(w).Encode(Response{
+					Code:    http.StatusOK,
+					Message: "Contact successfully created.",
+				})
+				return
+			}
+		}
+	}
+	json.NewEncoder(w).Encode(Response{
+		Code: http.StatusBadRequest,
+	})
 }
 
 func ListContact(w http.ResponseWriter, r *http.Request) {
--- data/contact.go
+++ data/contact.go
@@ -1,5 +1,10 @@
 package data
 
+import (
+	"rest-in-go/db"
+	"fmt"
+)
+
 type Contact struct {
 	ContactId     int    `json:"contact_id"`
 	UserId        int    `json:"user_id"`
@@ -7,8 +12,14 @@ type Contact struct {
 	ContactNumber string `json:"contact_number"`
 }
 
-func (c *Contact) Save() {
-
+func (c *Contact) Save() bool {
+	query := "CREATE TABLE IF NOT EXISTS contacts(contact_id INTEGER PRIMARY KEY AUTOINCREMENT, user_id INTEGER, contact_name TEXT, contact_number TEXT);"
+	if db.Exec(query) {
+		query = "INSERT INTO contacts(user_id, contact_name, contact_number) VALUES(%d, '%s', '%s');"
+		query = fmt.Sprintf(query, c.UserId, c.ContactName, c.ContactNumber)
+		return db.Exec(query)
+	}
+	return false
 }
 
 func (c *Contact) Delete() {
